36088579741b0379ee1859f9e677040e32f9d13b
arthurseredaa <neonbetcy@gmail.com>
2021-04-07 19:18:00 +0300

    Refactoring

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,23 +1,26 @@
 import { FC, useEffect, useState } from "react"
 import classes from './Game.module.css';
 import { PriceList } from "./PriceList/PriceList";
+import { QuestionHolder } from "./QuestionHolder/QuestionHolder";
 
 export const Game: FC = () => {
-  const [questions, setQuestions] = useState();
+  const [questions, setQuestions] = useState([]);
   const [money, setMoney] = useState<Array<{ money: string, current: boolean, done: false }>>([]);
+  const [stage, setStage] = useState(0)
 
   useEffect(() => {
     const fetchQuestions = async () => {
       let data = await fetch("./gameConfig.json").then(res => res.json());
-      const allMoney = data.questions.map((quess: any, index: number) => {
-        if (index === 0) {
-          return { money: quess.money, current: true, done: false }
-        } else {
-          return { money: quess.money, current: false, done: false }
-        }
-      });
+
+      const allMoney = data.questions.map((quess: any, index: number) => index === 0 ? { money: quess.money, current: true, done: false } : { money: quess.money, current: false, done: false });
+
+      const allQuestions = data.questions.map((quess: any) => ({ question: quess.question, answers: quess.answers }));
+
+      setQuestions(allQuestions)
 
       setMoney(allMoney);
+
+      console.log(allMoney)
     }
 
     fetchQuestions();
@@ -25,10 +28,8 @@ export const Game: FC = () => {
 
   return (
     <div className={classes.gameWrapper}>
-      <div>
-        here will be answers
-      </div>
-      <PriceList money={money} />
+      <QuestionHolder questions={questions} stage={stage} />
+      <PriceList money={money} stage={stage} />
     </div>
   )
 }
