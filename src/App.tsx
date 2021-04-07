c0a3ea4c5bcc9817d058129f9ded704571e1eca9
arthurseredaa <neonbetcy@gmail.com>
2021-04-07 12:43:47 +0300

    Create first template of homescreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import HandIcon from "./assets/images/hand.svg";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div>
+        <img src={HandIcon} alt="Hand" />
+        <div>
+          <h1>text</h1>
+          here need a button
+        </div>
+      </div>
     </div>
   );
 }
