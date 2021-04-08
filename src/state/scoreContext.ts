f87a945ea8ea342c4c71369d031501aa6d97a3d6
arthurseredaa <neonbetcy@gmail.com>
2021-04-08 19:01:12 +0300

    Create context

---
+++ src/state/scoreContext.ts
@@ -0,0 +1,12 @@
+import { createContext } from 'react';
+import { ScoreContext } from '../types/scoreContext';
+
+const SCORE_DEFAULT_VALUE = {
+  score: "0",
+  setCurrentScore: () => { },
+  stage: 0,
+  setCurrentStage: () => {},
+
+}
+
+export const TotalScoreContext = createContext<ScoreContext>(SCORE_DEFAULT_VALUE);
