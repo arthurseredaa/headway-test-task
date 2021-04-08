9e9c683577d115197b413705e2e36943dcf67a6c
arthurseredaa <neonbetcy@gmail.com>
2021-04-08 18:58:48 +0300

    End ga,e page

---
+++ src/components/EndGame/EndGame.tsx
@@ -0,0 +1,23 @@
+import { FC, useContext } from "react";
+import { useHistory } from "react-router"
+
+import { TotalScoreContext } from "../../state/scoreContext";
+import { Layout } from "../common/Layout/Layout"
+
+import { formatter } from "../../helpers/formatCurrency";
+
+
+export const EndGame: FC = () => {
+  const history = useHistory();
+  const { score, setCurrentScore, setCurrentStage } = useContext(TotalScoreContext);
+
+  const handleClick = () => {
+    history.push("/game");
+    setCurrentScore("0");
+    setCurrentStage(0)
+  }
+
+  let totalScoreTitle = `$${formatter(+score)} earned`
+
+  return <Layout titleText={totalScoreTitle} buttonText="Try again" clickHandler={handleClick} />
+}
