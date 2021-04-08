f87a202f14e759af4a93faaf0dfc16cd712211f2
arthurseredaa <neonbetcy@gmail.com>
2021-04-08 18:59:40 +0300

    Responsive design, use context

--- src/components/Game/PriceList/PriceList.tsx
+++ src/components/Game/PriceList/PriceList.tsx
@@ -1,17 +1,19 @@
-import { MoneyItem } from "../../../types";
+import { FC, useContext } from "react";
+
+import { TotalScoreContext } from "../../../state/scoreContext";
+import { PriceListProps } from "../../../types/game";
+import { MoneyItem } from "../../../types/moneyItem";
 import { PriceItem } from "./PriceItem/PriceItem";
+
 import classes from "./PriceList.module.css";
 
-interface PriceListProps {
-  money: Array<MoneyItem>,
-  stage: number
-}
+export const PriceList: FC<PriceListProps> = ({ money, openMenu }: PriceListProps) => {
+  const { stage } = useContext(TotalScoreContext);;
 
-export const PriceList = ({ money, stage }: PriceListProps) => {
   return (
-    <div className={classes.priceListWrapper}>
+    <div className={`${classes.priceListWrapper} ${openMenu ? classes.collapsedMenuOpen : classes.collapsedMenuClose}`}>
       {
-        money && money.map((item: MoneyItem, index: number) => <PriceItem key={item.money} item={item} current={index === stage}  />)
+        money && money.map((item: MoneyItem, index: number) => <PriceItem key={item.money} item={item} current={index === stage} />)
       }
     </div>
   )
