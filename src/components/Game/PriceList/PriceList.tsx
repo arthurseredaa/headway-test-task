cb5840cd3d773b2d8053ca77c867d8705149162a
arthurseredaa <neonbetcy@gmail.com>
2021-04-09 02:58:37 +0300

    Responsive price list

--- src/components/Game/PriceList/PriceList.tsx
+++ src/components/Game/PriceList/PriceList.tsx
@@ -1,4 +1,5 @@
-import { FC, useContext } from "react";
+import { FC, useContext, useEffect, useState } from "react";
+import { useWindowWidth } from "../../../hooks/useWindowWidth";
 
 import { TotalScoreContext } from "../../../state/scoreContext";
 import { PriceListProps } from "../../../types/game";
@@ -8,10 +9,20 @@ import { PriceItem } from "./PriceItem/PriceItem";
 import classes from "./PriceList.module.css";
 
 export const PriceList: FC<PriceListProps> = ({ money, openMenu }: PriceListProps) => {
-  const { stage } = useContext(TotalScoreContext);;
+  const { stage } = useContext(TotalScoreContext);
+  const [hideMenu, setHideMenu] = useState(false);
+  const size = useWindowWidth();
+
+  useEffect(() => {
+    if(size <= 1350) {
+      setHideMenu(true);
+    } else {
+      setHideMenu(false);
+    }
+  }, [size]);
 
   return (
-    <div className={`${classes.priceListWrapper} ${openMenu ? classes.collapsedMenuOpen : classes.collapsedMenuClose}`}>
+    <div className={`${classes.priceListWrapper} ${openMenu && classes.collapsedMenuOpen} ${!openMenu && hideMenu && classes.collapsedMenuClose}`}>
       {
         money && money.map((item: MoneyItem, index: number) => <PriceItem key={item.money} item={item} current={index === stage} />)
       }
