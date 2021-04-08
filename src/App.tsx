1fbb702fb6dc7a517c1b67eb5295136ffa734c4c
arthurseredaa <neonbetcy@gmail.com>
2021-04-08 18:57:44 +0300

    Add context provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,24 @@ import { FC } from "react";
 import { Route } from "react-router";
 
 import { Home } from "./components/Home/Home";
+import { Game } from "./components/Game/Game";
+import { TotalScoreContext } from "./state/scoreContext";
+import { useScore } from "./hooks/useScore";
+import { EndGame } from "./components/EndGame/EndGame";
 
 import './App.css';
-import { Game } from "./components/Game/Game";
 
-export const App: FC = () =>  {
+export const App: FC = () => {
+  const score = useScore();
+
   return (
-    <div className="app">
-      <Route exact path="/" component={Home} />
-      <Route exact path="/game" component={Game} />
-    </div>
+    <TotalScoreContext.Provider value={score}>
+      <div className="app">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/game" component={Game} />
+        <Route exact path="/end" component={EndGame} />
+      </div>
+    </TotalScoreContext.Provider>
   );
 }
 
